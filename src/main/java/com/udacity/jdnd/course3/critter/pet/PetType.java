b5aed5da0fa8100c01f5f3617651e52fbd460472
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-20 13:38:32 +0300

    feat: Add deserialization method

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetType.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetType.java
@@ -1,8 +1,23 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 /**
  * A example list of pet type metadata that could be included on a request to create a pet.
  */
 public enum PetType {
+
     CAT, DOG, LIZARD, BIRD, FISH, SNAKE, OTHER;
+
+    @JsonCreator
+    public static PetType fromString(String key) {
+        for (PetType type : PetType.values()) {
+            if (type.name().equalsIgnoreCase(key)) {
+                return type;
+            }
+        }
+
+        return OTHER;
+    }
+
 }
