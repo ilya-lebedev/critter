dafaf5b6dec2f5c9e21cf86d652a247e2a978578
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-24 14:18:02 +0300

    feat: Create employee service

---
+++ src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import org.springframework.stereotype.Service;
+
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Service
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee save(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeById(long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public void setAvailability(Set<DayOfWeek> daysAvailable, long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).get();
+        employee.setDaysAvailable(daysAvailable);
+        employeeRepository.save(employee);
+    }
+
+}
