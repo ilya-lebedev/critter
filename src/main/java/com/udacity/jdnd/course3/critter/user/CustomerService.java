e3ee5b6cd00c1c851b131383e920693f5c2f5747
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-25 10:43:54 +0300

    feat: Implement addPetToCustomer method

--- src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -1,10 +1,14 @@
 package com.udacity.jdnd.course3.critter.user;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
 
     private CustomerRepository customerRepository;
@@ -23,6 +27,18 @@ public class CustomerService {
 
     public Customer getCustomerById(long id) {
         return customerRepository.findById(id).get();
+    }
+
+    public void addPetToCustomer(Customer customer, Pet pet) {
+        List<Pet> pets = customer.getPets();
+
+        if (pets == null) {
+            pets = new ArrayList<>();
+        }
+
+        pets.add(pet);
+        customer.setPets(pets);
+        customerRepository.save(customer);
     }
 
 }
