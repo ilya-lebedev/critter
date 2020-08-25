2096f45b3a43dbd65247a20b66a9ca7f10e5cea4
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-25 11:25:21 +0300

    feat: Implement findCustomerByPetsId method

--- src/main/java/com/udacity/jdnd/course3/critter/user/CustomerRepository.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/CustomerRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Customer findCustomerByPetsId(Long petId);
+
 }
