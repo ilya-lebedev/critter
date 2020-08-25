a1e6eb5dccb7a2ac4e1d2c6cbf7f9a9d9ee748a3
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-25 16:22:57 +0300

    feat: Implement findEmployeesBySkillsAndDate method

--- src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeService.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -28,6 +31,14 @@ public class EmployeeService {
         Employee employee = employeeRepository.findById(employeeId).get();
         employee.setDaysAvailable(daysAvailable);
         employeeRepository.save(employee);
+    }
+
+    public List<Employee> findEmployeesBySkillsAndDate(Set<EmployeeSkill> skills, LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        List<Employee> employees = employeeRepository.getEmployeesByDaysAvailable(dayOfWeek);
+        return employees.stream()
+                .filter(employee -> employee.getSkills().containsAll(skills))
+                .collect(Collectors.toList());
     }
 
 }
