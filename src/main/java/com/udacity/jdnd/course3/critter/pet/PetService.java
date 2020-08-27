cfa755de4f9d5bee23e166cfa6bce0d23dc8653e
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-27 10:30:09 +0300

    feat: Implement getPetsByIds and addScheduleToPet methods

--- src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -1,9 +1,11 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.schedule.Schedule;
 import com.udacity.jdnd.course3.critter.user.CustomerService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -32,8 +34,24 @@ public class PetService {
         return petRepository.findById(id).get();
     }
 
+    public List<Pet> getPetsByIds(List<Long> petIds) {
+        return petRepository.findAllById(petIds);
+    }
+
     public List<Pet> getPetsByOwnerId(long ownerId) {
         return petRepository.findPetByCustomerId(ownerId);
+    }
+
+    public void addScheduleToPet(Pet pet, Schedule schedule) {
+        List<Schedule> schedules = pet.getSchedules();
+
+        if (schedules == null) {
+            schedules = new ArrayList<>();
+        }
+
+        schedules.add(schedule);
+        pet.setSchedules(schedules);
+        petRepository.save(pet);
     }
 
 }
