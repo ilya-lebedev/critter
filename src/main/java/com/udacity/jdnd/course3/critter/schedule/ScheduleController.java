6629f3f2203aa0b34530cdb004f82d597c50ba50
Ilya Lebedev <ilya.al.lebedev@gmail.com>
2020-08-27 10:35:48 +0300

    feat: Implement converters

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.pet.PetService;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Schedules.
@@ -10,6 +16,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
+
+    private ScheduleService scheduleService;
+    private PetService petService;
+    private EmployeeService employeeService;
+
+    public ScheduleController(ScheduleService scheduleService,
+                              PetService petService,
+                              EmployeeService employeeService) {
+        this.scheduleService = scheduleService;
+        this.petService = petService;
+        this.employeeService = employeeService;
+    }
 
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
@@ -34,6 +52,46 @@ public class ScheduleController {
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
         throw new UnsupportedOperationException();
+    }
+
+    private List<ScheduleDTO> convertScheduleListToScheduleDTOList(List<Schedule> schedules) {
+        return schedules.stream()
+                .map(this::convertScheduleToScheduleDTO)
+                .collect(Collectors.toList());
+    }
+
+    private List<Schedule> convertScheduleDTOListToScheduleList(List<ScheduleDTO> scheduleDTOs) {
+        return scheduleDTOs.stream()
+                .map(this::convertScheduleDTOToSchedule)
+                .collect(Collectors.toList());
+    }
+
+    private ScheduleDTO convertScheduleToScheduleDTO(Schedule schedule) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        BeanUtils.copyProperties(schedule, scheduleDTO);
+        List<Long> employeeIds = schedule.getEmployees()
+                .stream()
+                .map(Employee::getId)
+                .collect(Collectors.toList());
+        scheduleDTO.setEmployeeIds(employeeIds);
+        List<Long> petIds = schedule.getPets()
+                .stream()
+                .map(Pet::getId)
+                .collect(Collectors.toList());
+        scheduleDTO.setPetIds(petIds);
+
+        return scheduleDTO;
+    }
+
+    private Schedule convertScheduleDTOToSchedule(ScheduleDTO scheduleDTO) {
+        Schedule schedule = new Schedule();
+        BeanUtils.copyProperties(scheduleDTO, schedule);
+        List<Pet> pets = petService.getPetsByIds(scheduleDTO.getPetIds());
+        schedule.setPets(pets);
+        List<Employee> employees = employeeService.getEmployeesByIds(scheduleDTO.getEmployeeIds());
+        schedule.setEmployees(employees);
+
+        return schedule;
     }
 
 }
